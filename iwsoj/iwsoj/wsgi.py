688026f7d1535356c283eae4abc5e7006f5b421b
eryktr <eryk.trzeciakiewicz@gmail.com>
2019-10-12 01:01:54 +0200

    Updated docstring

--- iwsoj/iwsoj/wsgi.py
+++ iwsoj/iwsoj/wsgi.py
@@ -1,10 +1,5 @@
 """
-WSGI config for iwsoj project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
