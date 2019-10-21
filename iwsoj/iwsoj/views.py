9f6c2153545968a83f1ad2c755e2e1a229181255
Rafal1997 <rafal.mlodzieniak@o2.pl>
2019-10-21 23:17:43 +0200

    try co create basic frontend - page name

--- iwsoj/iwsoj/views.py
+++ iwsoj/iwsoj/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_201_CREATED
 
 
 def home(request):
     return HttpResponse("Nothing to find here. Use the /api tree.", status=HTTP_404_NOT_FOUND)
+
+
+def index(request):
+    return HttpResponse("Index page was created.", status=HTTP_201_CREATED)
+
+
+def login(request):
+    return HttpResponse("Login page was created.", status=HTTP_201_CREATED)
+
+
+def register(request):
+    return HttpResponse("Register page was created.", status=HTTP_201_CREATED)
