d6b1e963d197bf430f3fa7bd42867eb9cff6af3b
drzewiec123 <pawelsosna192@gmail.com>
2019-10-28 19:23:00 +0100

    Added tests for stdin inside Doocker

--- iwsoj/runner/imgs/c/run.sh
+++ iwsoj/runner/imgs/c/run.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd /runner
-gcc target.c -o ./exec
-chmod +x ./exec
-./exec
+cd /runner;
+gcc target.c -o ./exec;
+chmod +x ./exec;
+./exec < /runner/input.txt;
--- iwsoj/tests/runner/test_runner.py
+++ iwsoj/tests/runner/test_runner.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
+import os
 import pytest
 
 from runner.error import UnsupportedLangError
-from runner.runner import Lang, get_dockerfile_dir
+from runner.runner import Lang, get_dockerfile_dir, soSorryYouLose
 
 
 def test_lang_fromfile_ok():
@@ -20,4 +23,37 @@ def test_lang_tostring():
 
 
 def test_get_dockerfile_dir():
-    assert get_dockerfile_dir(Lang.C).endswith("iwsoj/runner/imgs/c")
+    sep = os.sep
+    assert get_dockerfile_dir(Lang.C).endswith("iwsoj" + sep + "runner" + sep + "imgs" + sep + "c")
+
+
+dummypath = Path.cwd() / 'runner' / 'dummy'
+
+
+@pytest.mark.integration
+def test_runner_c_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy.c')) == "It takes 8 bits to represent 222\n"
+
+
+@pytest.mark.integration
+def test_runner_cpp_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy.cpp')) == "Distance from p1 and p2 is 0\n"
+
+
+@pytest.mark.integration
+def test_runner_py3_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy.py')) == "The 10th Fib number is 55\n"
+
+
+@pytest.mark.integration
+def test_runner_java_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy.java')) == "Factorial of 4 is 24\n"
+
+
+@pytest.mark.integration
+def test_runner_go_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy.go')) == "GO somewhere else!\n"
+
+@pytest.mark.integration
+def test_runner_c_stdin_ok():
+    assert soSorryYouLose(str(dummypath / 'dummy_stdin.c')) == "It takes 8 bits to represent 220\n"
