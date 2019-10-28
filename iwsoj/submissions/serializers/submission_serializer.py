0c8d8434ba038688a7ca197abcaefd7402a4495a
drzewiec123 <pawelsosna192@gmail.com>
2019-10-28 19:23:16 +0100

    Committed everything again

--- iwsoj/submissions/serializers/submission_serializer.py
+++ iwsoj/submissions/serializers/submission_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from submissions.models import Submission
+#from submissions.tester.tester import validate_code
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
@@ -14,10 +15,15 @@ class SubmissionSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data) -> Submission:
+        sourceCode = validated_data['sourceCode']
+        task = validated_data['task']
+        language = validated_data["language"]
+        status = 0 #validate_code(sourceCode, language, task)
         submission = Submission.objects.create(
-            sourceCode=validated_data["sourceCode"],
-            language=validated_data["language"],
-            task=validated_data['task'],
-            user=self.context['request'].user
+            sourceCode=sourceCode,
+            language=language,
+            task=task,
+            user=self.context['request'].user,
+            status=status
         )
         return submission
