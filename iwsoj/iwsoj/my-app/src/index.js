c684f526165c95cb84c6c9952844dcc9cd41e3e2
Rafal1997 <rafal.mlodzieniak@o2.pl>
2019-10-31 15:52:49 +0100

    Try to create a simple server

---
+++ iwsoj/iwsoj/my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
+import './index.css';  
+import App from './App';  
+import About from './about'  
+import Contact from './contact'  
+  
+const routing = (  
+  <Router>  
+    <div>  
+      <h1>React Router Example</h1>  
+      <Route exact path="/" component={App} />  
+      <Route path="/about" component={About} />  
+      <Route path="/contact" component={Contact} />  
+    </div>  
+  </Router>  
+)  
+//ReactDOM.render(routing, document.getElementById('root'));  
