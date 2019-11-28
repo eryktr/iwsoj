7d8b0e530730aec2c20342d363fa04bd8fdfd4c1
Rafal1997 <rafal.mlodzieniak@o2.pl>
2019-11-28 20:51:34 +0100

    prime_numbers in four languages

---
+++ iwsoj/runner/alg_languages/prime_numbers.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int prime(int n) {
+  if (n<2) {
+    return 0;
+  } else {
+    int counter = 0;
+    for (int i=2; i<=n; i++) {
+      counter++;
+      for (int j=2; j<=sqrt(i); j++) {
+        if (i%j==0) {
+          counter--;
+          break;
+		}
+      }
+    }
+    return counter;
+  }       
+	
+}
+
+int main(void)
+{
+	int n;
+	while (scanf("%d\n",&n)!=EOF) {
+		cout << prime(n) << endl;
+	}
+
+}
